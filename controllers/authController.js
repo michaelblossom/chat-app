0e963db231b0f55754d6db657c56d692a4164c29
michael <michaelblossom8654@gmail.com>
2024-06-23 02:18:05 +0530

    ci:implimented a function to create and send token

---
+++ controllers/authController.js
@@ -0,0 +1,41 @@
+const crypto = require("crypto");
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const sendEmail = require("./../utils/email");
+
+// function to generate token
+const signToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: "90d",
+  });
+};
+
+// function to create and send token to client
+const createAndSendToken = (user, statusCode, res) => {
+  // calling signToken function to generate token
+  const token = signToken(user._id);
+  const cookiesOptions = {
+    expiresIn: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPJIRES_IN * 24 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") cookiesOptions.secure = true; //we only want secure option when we are in production
+  // creating a cookie
+  res.cookie("jwt", token, cookiesOptions);
+
+  // removing password field when a user is signedup
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token, //sending the token to the user
+    data: {
+      user: user,
+    },
+  });
+};
