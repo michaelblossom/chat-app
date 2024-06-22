137706cd6042252c7a85ace991aaaabb2c82b995
michael <michaelblossom8654@gmail.com>
2024-06-23 03:13:08 +0530

    ci:implimented resetPassword endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -152,3 +152,48 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     );
   }
 });
+
+//RESET PASSWORD
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) get user based on the token
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+  // 2) if token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError("Token is invalid or has expired", 404));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+  // 3) Update changedPassword property for the user
+  //this step was done in the user model
+  // 4) Log the user in, send JWT
+  createAndSendToken(user, 200, res);
+});
+// updating password
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1)get the user from the collection
+  const user = await User.findById(req.user.id).select("+password");
+  // 2)check if the posted pasted password is correct
+  // calling correctpassword function from usermodel
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError("your current password is wrong", 401));
+  }
+  // 3)if if the posted password is correct, update the user
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save({ validateBeforeSave: false });
+  // 4)Log user in, send jwt
+  // calling the createAndSendToken function
+  createAndSendToken(user, 200, res);
+});
