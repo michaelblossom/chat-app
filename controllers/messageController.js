f14fd565a742ce5abb757bba480fe352046f9b58
michael <michaelblossom8654@gmail.com>
2024-06-23 12:23:35 +0530

    implimented edit message endpoint

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,12 +2,42 @@ const Message = require("../models/message");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.createMessage = catchAsync(async (req, res, next) => {
   const newMessage = await Message.create(req.body);
   res.status(201).json({
     status: "success",
     data: {
       message: newMessage,
+    },
+  });
+});
+
+exports.editMessage = catchAsync(async (req, res, next) => {
+  const filteredBody = filterObj(req.body, "message");
+  const message = await Message.findOne({
+    $and: [{ user: req.user.id }, { _id: req.params.id }],
+  });
+  if (!message) {
+    return next(new appError("No message found ", 404));
+  }
+
+  const newMessage = await Message.findByIdAndUpdate(message.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      newMessage,
     },
   });
 });
